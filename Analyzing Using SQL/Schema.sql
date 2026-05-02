e2f01e13712262d63a6620d246433dd1ca0d6193
sameerarhan <73331454+sameerarhan@users.noreply.github.com>
2026-05-02 22:07:20 +0530

    Update and rename Create Table.sql to Schema.sql

---
+++ Analyzing Using SQL/Schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE mla_list (
+    Candidate_id INT PRIMARY KEY,
+    Candidate VARCHAR(255),
+    Constituency VARCHAR(255),
+    Party VARCHAR(100),
+    Age INT,
+    Gender ENUM('MALE','FEMALE'),
+    Criminal_Cases INT,
+    Serious_Criminal_Cases ENUM('Yes','No'),
+    Education VARCHAR(100),
+    Total_Assets DECIMAL(18,2),
+    Liabilities DECIMAL(18,2),
+    Category ENUM('GENERAL','SC','ST')
+);
+
+
